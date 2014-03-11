ded399932b43a4b950c16a41e89ebdf83b90c442
rehanb <rehan.balagam@gmail.com>
2014-03-10 21:25:33 -0500

    moved www directory and also updated the code...
    use this command instead:
    java -Djavax.net.ssl.keyStore=server.jks
    -Djavax.net.ssl.keyStorePassword=testing HttpsServer --port=9096

--- src/HttpsServer.java
+++ src/HttpsServer.java
@@ -24,7 +24,7 @@ public class HttpsServer extends Thread {
 	    ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
             ServerSocket serverSocket = ssf.createServerSocket(Integer.parseInt(arrInput[1]));
 
-	    SSLContext sslc = SSLContext.getInstance("TLS");	  
+/*	    SSLContext sslc = SSLContext.getInstance("TLS");	  
 	    char [] pswd = "testing".toCharArray();
 	    KeyStore ks = KeyStore.getInstance("JKS");
 	    FileInputStream fin = new FileInputStream(ks);
@@ -33,7 +33,7 @@ public class HttpsServer extends Thread {
 	    TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
 	    tmf.init(ks);
 	    sslc.init(kmf.getKeyManagers(),tmf.getTrustManagers(), new SecureRandom());
-
+*/
 	    while (true) {
 	    	new HttpsServer(serverSocket.accept()).start();
 	    }
