abfb4810095cda862d90b04d7728f6ebbcb409e2
Steve Bartfield <Stevo393@gmail.com>
2014-03-16 17:39:35 -0500

    UPDATED with persistant connection code

--- src/Server.java
+++ src/Server.java
@@ -1,182 +1,266 @@
-/****************
- *Steven Bartfield and Rehan Balagamwala- MPCS 54001 - Project 1
- ****************/
-
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.security.*;
+import javax.net.*;
+import javax.net.ssl.*;
 
-public class Server {
+public final class Server {
+    private final int serverPort;
+    private Map<String, byte[]> resourceMap;
+    private Map<String, String> redirectMap;
+    private ServerSocket socket;
+    private DataOutputStream toClientStream;
+    private DataInputStream fromClientStream;
 
-    public static void main(String[] args) throws Exception {
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
 
-        //------------------------------------------------------------------------------
-        //Setting up the sockets and defining variables
-        //------------------------------------------------------------------------------
-        String[] arrInput = args[0].split("=");            //parse out the port number
+    public void loadResources() throws IOException {
+        resourceMap = ResourceMap.loadFiles();
+        redirectMap = ResourceMap.loadRedirects();
+    }
 
-        while (true){    //will continue to look for requests
+    /**
+     * Creates a socket + binds to the desired server-side port #.
+     *
+     * @throws {@link IOException} if the port is already in use.
+     */
+    public void bind() throws IOException {
+        socket = new ServerSocket(serverPort);
+        System.out.println("Server bound and listening to port " + serverPort);
+    }
 
-            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(arrInput[1]));
+    public void bindSSL() throws IOException {
+        try {
+            SSLContext sslc = SSLContext.getInstance("TLS");
+            char [] pswd = "testing".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fin = new FileInputStream("server.jks");
+            ks.load(fin,pswd);
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, pswd);
+            sslc.init(kmf.getKeyManagers(), null, null);
+            SSLServerSocketFactory sslf = sslc.getServerSocketFactory();
+            SSLServerSocket serverSocket;
+            socket = (SSLServerSocket) sslf.createServerSocket(serverPort);
+            System.out.println("Server bound and listening to port " + serverPort);
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
 
-            boolean persistent = false;
-            Socket persist = null;
+    /**
+     * Waits for a client to connect, and then sets up stream objects for communication
+     * in both directions.
+     *
+     * @return The newly-created client {@link Socket} if the connection is successfully
+     *     established, or {@code null} otherwise.
+     * @throws {@link IOException} if the server fails to accept the connection.
+     */
+    public Socket acceptFromClient() throws IOException {
+        Socket clientSocket;
+        try {
+            clientSocket = socket.accept();
 
-            Socket clientSocket;
-            clientSocket = serverSocket.accept();
+
+        } catch (SecurityException e) {
+            System.out.println("The security manager intervened; your config is very wrong. " + e);
+            return null;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Probably an invalid port number. " + e);
+            return null;
+        } catch (IOException e) {
+            System.out.println("IOException in socket.accept()");
+            return null;
+        }
+
+        try {
+            toClientStream = new DataOutputStream(clientSocket.getOutputStream());
+            fromClientStream = new DataInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("exception creating the stream objects.");
+        }
+        return clientSocket;
+    }
+
+    public void handleRequest() throws IOException {
+        List<String> rawRequest = new ArrayList<String>();
+
+        String inputLine;
+        do {
+            inputLine = fromClientStream.readLine();
+
+            //if (inputLine == null) {
+            //	System.out.println("inputLine was null!\n");
+            //		break;
+            //	}
+
+            rawRequest.add(inputLine);
+
+        } while ((inputLine != null) && (inputLine.length() > 0));
+
+        System.out.println(String.format("[%s]", rawRequest));
+        HTTPRequest request = new HTTPRequest(rawRequest);
+        System.out.println(request);
 
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-            //read in top line, if empty then wait for user request
-            String strInput = in.readLine();
-            if (strInput.isEmpty()){ continue; }
 
-            //------------------------------------------------------------------------------
-            //Parsing out request and building the header file
-            //------------------------------------------------------------------------------
 
-            //Parse out the requested file from the client request string
-            String strRequestType = strInput.substring(0,4); //pulls the request out
-            String strPath = null;
+        // TODO(ajn): support POST along with GET/HEAD
+        if (request.getType() != HTTPRequest.Command.GET &&
+                request.getType() != HTTPRequest.Command.HEAD) {
+            send403(request, String.format("%s not supported.", request.getType()));
+            return;
+        }
 
-            //check for request type and handle accordingly
-            if (strRequestType.equals("GET ")){
-                strPath = strInput.substring(5, strInput.indexOf("HTTP") - 1);
-            }
-            if (strRequestType.equals("HEAD")){
-                strPath = strInput.substring(6, strInput.indexOf("HTTP") - 1);
-            }
+        // See if this is supposed to be a redirect, first.
+        if (redirectMap.containsKey(request.getPath())) {
+            send301(request, redirectMap.get(request.getPath()));
+        } else if (!resourceMap.containsKey(request.getPath())) {
+            send404(request);
+        } else {
+            byte[] content = resourceMap.get(request.getPath());
+            send200(request, content);
+        }
+    }
 
-            //Create the header and send back to client
-            String strHeader = createHeader(strPath, strRequestType);
-            out.writeBytes(strHeader + "\r\n");
+    private void send301(HTTPRequest request, String newUrl) throws IOException {
+        String responseBody = new StringBuilder()
+                .append("<HTML><HEAD><TITLE>301 Moved</TITLE></HEAD>\r\n")
+                .append("<BODY><H1>These aren't the droids you're looking for.</H1>\r\n")
+                .append(String.format("This resource has moved <A HREF=\"%s\">here</A>.\r\n", newUrl))
+                .append("</BODY></HTML>\r\n")
+                .toString();
 
-            //------------------------------------------------------------------------------
-            //Handling the rest of the file request -- will fail if there is no file to be transmitted (404 was delivered)
-            //------------------------------------------------------------------------------
+        StringBuilder response = new StringBuilder()
+                .append("HTTP/1.1 301 Moved Permanently\r\n")
+                .append(String.format("Location: %s\r\n", newUrl))
+                .append(String.format("Content-Type: text/html; charset=UTF-8\r\n"))
+                .append("Connection: close\r\n")
+                .append(String.format("Content-Length: %d\r\n", responseBody.length()));
+        if (request.getType() == HTTPRequest.Command.GET) {
+            response.append(String.format("\r\n%s", responseBody));
+        }
+        toClientStream.writeBytes(response.toString());
+    }
 
-            //only retrieve if a GET file and not a redirect
-            if (strRequestType.equals("GET ") && (!strHeader.substring(9,12).equals("301")))    {
-                try{
-                    //get the file per the request and input it into the file stream
-                    File fileRequested = new File(strPath);
-                    FileInputStream fileOutbound = new FileInputStream(fileRequested);
+    private void send404(HTTPRequest request) throws IOException {
+        String responseBody = new StringBuilder()
+                .append("<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\r\n")
+                .append("<BODY><H1>I can't find any resource of the name \r\n")
+                .append(String.format("[%s] on this server.\r\n", request.getPath()))
+                .append("</BODY></HTML>\r\n")
+                .toString();
 
-                    //preparing the file to be transmitted
-                    int nByteSize = (int) fileRequested.length();  //find the number bytes in the file
-                    byte[] bytFile = new byte[nByteSize];  //putting the file into bytes
-                    fileOutbound.read(bytFile);  //read the bytes into a new file to be sent out
+        StringBuilder response = new StringBuilder()
+                .append("HTTP/1.1 404 Not Found\r\n")
+                .append("Content-Type: text/html; charset=UTF-8\r\n")
+                .append("Connection: keep-alive\r\n")
+                .append(String.format("Content-Length: %d\r\n", responseBody.length()));
+        if (request.getType() == HTTPRequest.Command.GET) {
+            response.append(String.format("\r\n%s\r\n", responseBody));
+        }
+        try {
+            toClientStream.writeBytes(response.toString());
+        } catch (IOException e) {
+            System.out.println("Client closed the socket before we finished the whole message.");
+        }
+    }
 
-                    //Transmit the data to client
-                    out.write(bytFile, 0, nByteSize);
-                }catch(Exception e){
-                    out.writeBytes("404. That's an Error! The requested URL was not found on this server. That's all we know.\r\n"); //for 404 requests //find out what should actually be written in the body for 404s.. if anything?
+    private void send403(HTTPRequest request, String errorDetail) throws IOException {
+        StringBuilder response = new StringBuilder()
+                .append("HTTP/1.1 403 Forbidden\r\n")
+                .append("Connection: keep-alive\r\n")
+                .append(String.format("Context-Length: %d\r\n", errorDetail.length()));
+        if (request.getType() == HTTPRequest.Command.GET) {
+            response.append(String.format("\r\n%s\r\n", errorDetail));
+        }
+        toClientStream.writeBytes(response.toString());
+    }
+
+    private void send200(HTTPRequest request, byte[] content) throws IOException {
+        StringBuilder response = new StringBuilder()
+                .append("HTTP/1.1 200 OK\r\n")
+                .append("Content-Type: text/html; charset=utf-8\r\n")
+                .append("Server: project1\r\n")
+                .append("Connection: keep-alive\r\n")
+                .append(String.format("Content-Length: %d\r\n", content.length));
+        toClientStream.writeBytes(response.toString());
+        if (request.getType() == HTTPRequest.Command.GET) {
+            toClientStream.writeBytes("\r\n");
+            ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+            outByteStream.write(content, 0, content.length);
+            outByteStream.writeTo(toClientStream);
+        }
+    }
+
+    public static void main(String argv[]) {
+        Map<String, String> flags = Utils.parseCmdlineFlags(argv);
+        if (!flags.containsKey("--serverPort")) {
+            System.out.println("usage: Server --serverPort=12345");
+            System.exit(-1);
+        }
+
+        int serverPort = -1;
+        try {
+            serverPort = Integer.parseInt(flags.get("--serverPort"));
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number! Must be an integer.");
+            System.exit(-1);
+        }
+
+        Server server = new Server(serverPort);
+        try {
+            server.loadResources();
+
+            //server.bindSSL();
+            server.bind();
+
+
+            while(true) {
+
+                System.out.println("pop");
+                Socket clientSocket = server.acceptFromClient();
+                //clientSocket.setKeepAlive(true);
+                //clientSocket.setSoTimeout(2000000);
+                System.out.println("hop");
+
+                if (clientSocket != null && clientSocket.isConnected()) {
+                    try {
+                        server.handleRequest();
+                    } catch (IOException e) {
+                        System.out.println("IO exception handling request, continuing.");
+                    }
+
+                    System.out.println("slop");
+                    try {
+                        server.handleRequest();
+                    } catch (IOException e) {
+                        System.out.println("IO exception handling request, continuing.");
+                    }
+                    System.out.println("flop");
+//                    try {
+//                        clientSocket.close();
+//                    } catch (IOException e) {
+//                        System.out.println("it's ok; the server already closed the connection.");
+//                    }
                 }
+//
+//              while (clientSocket.getSoTimeout() > 0){
+//                  server.handleRequest();
+//              }
+                //server.handleRequest();
+
             }
-
-            //Remove the rest of client's header from the buffer
-            while ( (strInput = in.readLine()) != null){
-                //System.out.println(strInput); //printing out full request for debug purposes
-                if (strHeader.contains("Connection: keep-alive")){
-                    persistent = true;
-                    persist = clientSocket;
-                    continue;
-                }
-
-
-                if (strInput.equals("") ){ break;} //breaks once the current request has ended
-            }
-
-            //------------------------------------------------------------------------------
-            //Closing off the current connection
-            //------------------------------------------------------------------------------
-
-            out.close();
-            in.close();
-            serverSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error communicating with client. aborting. Details: " + e);
         }
     }
-
-    //------------------------------------------------------------------------------
-    //Methods to help build appropriate headers
-    //------------------------------------------------------------------------------
-
-    //Method creates the header for the inputted path request
-    public static String createHeader(String strPathInput, String strRequestType){
-        String strHeader; //create string variable
-
-        //if not valid request (i.e. POST) send 403 error
-        if (strRequestType.equals("POST")){
-            strHeader =  "HTTP/1.1 403 Forbidden \r\n";
-            return strHeader;
-        }
-
-        //If invalid request, send 501 error
-        if (!strRequestType.equals("GET ") && !strRequestType.equals("HEAD")){
-            strHeader =  "HTTP/1.1 501 Not Implemented \r\n";
-            return strHeader;
-        }
-
-        File fileRequested = new File(strPathInput); //gets file requested to find length
-
-        //check if should redirect request
-        String strRedirectPath = findRedirect(strPathInput);
-        if (!strRedirectPath.isEmpty()){
-            strHeader =  "HTTP/1.1 301 Moved Permanetly \r\n";
-            strHeader += "Location: " + strRedirectPath + "\r\n";
-            return strHeader;
-        }
-
-
-        //check if file exists, if not then send 404
-        if (!fileRequested.exists()){
-            strHeader =  "HTTP/1.1 404 Not Found \r\n";
-            strHeader += "Content-Length: 91 \r\n";
-            strHeader += "Content-Type: text/html \r\n";
-            return strHeader;
-        }
-
-        //for paths that exist(redirect to be coded in future)
-        strHeader =  "HTTP/1.1 200 OK \r\n";
-        strHeader += "Content-Length: " + fileRequested.length() + "\r\n";
-        strHeader += "Content-Type: " + findContentType(strPathInput) + "\r\n";
-        return strHeader;
-    }
-
-
-    //returns the content type file of the path request
-    public static String findContentType(String strPathInput){
-        String strContentType = ""; //initialize the variable
-        if      (strPathInput.endsWith("html")){strContentType = "text/html";}
-        else if (strPathInput.endsWith("txt")){strContentType = "text/plain";}
-        else if (strPathInput.endsWith("jpeg")){strContentType = "image/jpeg";}
-        else if (strPathInput.endsWith("png")){strContentType = "image/html";}
-        else if (strPathInput.endsWith("pdf")){strContentType = "application/pdf";}
-        else if (strPathInput.endsWith("exe")){strContentType = "application/octet-stream";} //need to look into
-        return strContentType;
-    }
-
-    //Checks to see if requested path is a redirect, if so returns redirect path
-    public static String findRedirect(String strPathInput){
-        String strRedirectPath = "";
-        Scanner scanRedirect;
-        try {scanRedirect = new Scanner(new File("www/redirect.defs")); }
-        catch(IOException e){
-            System.out.println("redirect.defs file could not be found" + e.getMessage());
-            return strRedirectPath; //send empty string (could not find file)
-        }   //only works if server in root
-
-        while (scanRedirect.hasNextLine()){
-            String strRedirect = scanRedirect.nextLine();
-            String[] strSplit = strRedirect.split(" ");
-            if (strSplit[0].equals("/" + strPathInput)){    //added "/" as this is how file is formatted
-                return strSplit[1];
-            }
-        }
-        return strRedirectPath; //no redirect path
-    }
-
 }
+
